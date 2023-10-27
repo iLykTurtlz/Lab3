2305ba0856068d1dfb97e3e828692a6c6462c34c
iLykTurtlz <pjarski@sfcm.edu>
2023-10-27 12:31:06 -0700

    random_forest.py implemented

--- random_forest.py
+++ random_forest.py
@@ -5,6 +5,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import sys
 from preprocessing import get_data
+from validation import *
 
 def main():
     argc = len(sys.argv)
@@ -12,11 +13,15 @@ def main():
         print("Usage: python3 randomForest.py <TrainingSetFile.csv> <NumAttributes> <NumDataPoints> <NumTrees>")
         sys.exit()
 
-    training_file = sys.argv[1]
-    num_attributes = int(sys.argv[2])
-    num_data_points = int(sys.argv[3])
-    num_trees = int(sys.argv[4])
-    
+    try:
+        training_file = sys.argv[1]
+        num_attributes = int(sys.argv[2])
+        num_data_points = int(sys.argv[3])
+        num_trees = int(sys.argv[4])
+    except:
+        print("Couldn't read one or more input parameters.")
+        sys.exit()
+
     X=None
     y=None
     # Load the dataset
@@ -37,4 +42,29 @@ def main():
     
     # Prepare the results file
     results_file = open('results.csv', 'w') 
+
+    forest = RandomForestClassifier(num_attributes, num_data_points, num_trees)
+    
+    k=10 #ten-fold
+    matrix, accuracies, avg_accuracy = cross_validation(forest, X, y, k, threshold=0.01, ratio=True)
+    print("Confusion matrix:")
+    print(matrix)
+    class_names = np.unique(y)
+    class_names.sort()
+
+    print("Accuracy per crossval iteration:", accuracies)
+    print("Average accuracy:",avg_accuracy)
+    
+    precisions, recalls = precision_recall_by_class(matrix)
+
+    print("Class labels:",class_names)
+    print("Precision by class:",precisions)
+    print("Recalls by class:", recalls)
+    f_measures = [f_measure(p,r) for p,r in zip(precisions, recalls)]
+    print("f-measures by class:",f_measures)
+
+    plot_confusion_matrix(matrix, class_names, precisions, recalls, f_measures)
+
+    print()
+
     
